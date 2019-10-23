11658a30938553bf5071052d8f1c04ac24596632
li Ethan <lixw1021@gmail.com>
2019-10-23 21:24:10 +0100

    initial custom view activity

--- app/src/main/java/com/xianwei/leanringlib/MainActivity.kt
+++ app/src/main/java/com/xianwei/leanringlib/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.xianwei.leanringlib.lib_android.animation.AnimationTestActivity
+import com.xianwei.leanringlib.lib_android.customview.CustomeViewActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
 
         animation_button.setOnClickListener {
             startActivity(Intent(this, AnimationTestActivity::class.java))
+        }
+
+        custom_view_button.setOnClickListener {
+            startActivity(Intent(this, CustomeViewActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/xianwei/leanringlib/lib_android/customview/CustomeViewActivity.kt
@@ -0,0 +1,16 @@
+package com.xianwei.leanringlib.lib_android.customview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.xianwei.leanringlib.R
+
+class CustomeViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_custom_view)
+
+    }
+
+}
